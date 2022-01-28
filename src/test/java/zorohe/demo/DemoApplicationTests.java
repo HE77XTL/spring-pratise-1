49f812fbccae2f83204a40b1d4d0e27cea5eb370
何东升 <he77505357@163.com>
2022-01-28 16:15:05 +0800

    spring-web-get

---
+++ src/test/java/zorohe/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package zorohe.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
